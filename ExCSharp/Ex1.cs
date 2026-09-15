e9542d227b85d753e84187a5599deb64ab814c3f
Long <vtran3331@gmail.com>
2026-09-16 01:22:20 +0700

    First ex

---
+++ ExCSharp/Ex1.cs
@@ -0,0 +1,23 @@
+using System;
+namespace ExCSharp
+{
+       public class Ex1
+    {
+        public static void Run()
+        {
+            //Step 1: Notify Input name
+            //Using Console.Write() to print word on screen (Not line break)
+            Console.Write("Please input your name please");
+            
+            //Step 2: Typing in4 from keyboard
+            //Using Console.ReadLine() read a string from user type in
+            //"Hoten" variable have type is string
+            string HoTen = Console.ReadLine();
+
+            //Step 3: Print result in screen
+            //Use Console.WriteLine() auto line break
+            // Use operator '+' to concatenate annoucement with "HoTen"
+            Console.WriteLine("Ho va ten ban vua nhap la: " + HoTen);
+        }
+    }
+}
---
+++ ExCSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace ExCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Teacher please chooose which exercise u want to run
+            Console.WriteLine("=== TONG HOP BAI TAP ===");
+            Console.WriteLine("Chon bai muon chay (1 - 15): ");
+            
+            // Convert the input string into an integer (int)
+            // Use int.TryParse to handle cases where the user enters random letters instead of a number
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                //switch case: checking result and choose one of case
+                switch(choice)
+                {
+                    case 1:
+                        Ex1.Run();
+                        Console.WriteLine("Chuong trinh Baitap1 dang chay");
+                        break;
+                    case 2:
+                       
+                        Console.WriteLine("Chuong trinh Baitap1 dang chay");
+                        break;
+                    default:
+                        Console.WriteLine("Loi: Chi co cac bai tap tu 1 den 15 thoi!");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Loi:Vui long nhap dung so luong bai tap 1-15");
+            }
+                Console.WriteLine("\nNhan phím bat ky de thoat...");
+                Console.ReadKey();
+        }
+    }
+}
